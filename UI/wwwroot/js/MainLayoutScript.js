e5dd5be9626bf8825932aa3808c62ec94f1594d7
prcstak <g.marsel2002@gmail.com>
2022-03-08 21:31:28 +0300

    sidebar label nav items play bar section body background custom scroll

---
+++ UI/wwwroot/js/MainLayoutScript.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function(){
+    $('.link').click(function(){
+        if($(this).parent().hasClass('link-active')){
+            return false;
+        }
+        $('.link').removeClass('link-active');
+        $(this).addClass('link-active');
+    });
+});
