0518b5a7623b83c887f5b4f42cae5eea9e52cc65
Марсель Гилязетдинов <MAGilyazetdinov@stud.kpfu.ru>
2022-03-16 23:32:35 +0300

    track

---
+++ UI/wwwroot/js/PlaybarScript.js
@@ -0,0 +1,8 @@
+﻿let wrapper = document.querySelector('.title'),
+    marquee = document.querySelector('.marquee'),
+    wrapperWidth = wrapper.offsetWidth,
+    marqueeWidth = marquee.scrollWidth;
+function move() {
+
+}
+var interval = setInterval(move, 5);
