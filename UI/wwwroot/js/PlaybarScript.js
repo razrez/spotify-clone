cb4fdb2786fe29f7cc8018d4893de2c3a6213c1d
Марсель Гилязетдинов <MAGilyazetdinov@stud.kpfu.ru>
2022-03-17 13:40:13 +0300

    control items

--- UI/wwwroot/js/PlaybarScript.js
+++ UI/wwwroot/js/PlaybarScript.js
@@ -1,8 +1,1 @@
-﻿let wrapper = document.querySelector('.title'),
-    marquee = document.querySelector('.marquee'),
-    wrapperWidth = wrapper.offsetWidth,
-    marqueeWidth = marquee.scrollWidth;
-function move() {
-
-}
-var interval = setInterval(move, 5);
+﻿
