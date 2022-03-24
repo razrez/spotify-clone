214daf8ffaec6b32af33a792570d83983dde9098
me <me>
2022-03-24 19:11:47 +0300

    different styles of playlist

--- UI/wwwroot/js/cards.js
+++ UI/wwwroot/js/cards.js
@@ -1,10 +1,11 @@
 ﻿class Card {
-    constructor(img, title, date, type, id) {
+    constructor(img, title, date, type = "", id, user = "") {
         this.img = img;
         this.title = title;
-        this.date = date;
+        this.date = !date ? "" : date + " •";
         this.type = type;
         this.id = id;
+        this.user = user;
     }
     
     render() {
@@ -13,13 +14,19 @@
         card.addEventListener("click", () => {
             window.location.href = `/album/${this.id}`;
         });
+        
+        let cover = !this.user ? 
+            `<h2 class="cover-description">${this.date} ${this.type}</h2>` :
+            `<h2 class="cover-description">By ${this.user} 
+                <span class="playlist-marker">${this.type}</span>
+            </h2>`
         card.innerHTML = ` 
             <div class="cover">
                 <img src="../img/${this.img}" alt="" class="watermark"/>
                 <div class="button-card"></div> <!--can be made with "card-active" class-->
                 <div class="cover-text">
                     <h1 class="cover-title">${this.title}</h1>
-                    <h2 class="cover-description">${this.date} • ${this.type}</h2>
+                    ${cover}
                 </div>
             </div>`
         
