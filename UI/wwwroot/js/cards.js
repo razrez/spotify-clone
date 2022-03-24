9d4a646c13db56924f603514cf03a85aaa32465c
me <me>
2022-03-24 20:03:45 +0300

    animated play button on card

--- UI/wwwroot/js/cards.js
+++ UI/wwwroot/js/cards.js
@@ -10,9 +10,15 @@
     
     render() {
         const card = document.createElement("div");
+            
         card.className = "card";
-        card.addEventListener("click", () => {
-            window.location.href = `/album/${this.id}`;
+        card.addEventListener("click", (e) => {
+            if (e.target && e.target.classList.contains("button-on-card")) {
+                e.stopPropagation();
+                e.target.classList.toggle("card-active")
+            }
+            else 
+                window.location.href = `/album/${this.id}`;
         });
         
         let cover = !this.user ? 
@@ -20,10 +26,11 @@
             `<h2 class="cover-description">By ${this.user} 
                 <span class="playlist-marker">${this.type}</span>
             </h2>`
+        
         card.innerHTML = ` 
             <div class="cover">
                 <img src="../img/${this.img}" alt="" class="watermark"/>
-                <div class="button-card"></div> <!--can be made with "card-active" class-->
+                <div class="button-on-card"></div> <!--can be made active with "card-active" class-->
                 <div class="cover-text">
                     <h1 class="cover-title">${this.title}</h1>
                     ${cover}
