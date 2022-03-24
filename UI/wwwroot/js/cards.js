0eb252f2c351bb70716d6ae5378bc1d51db78e9d
me <me>
2022-03-24 20:35:34 +0300

    added documentation

--- UI/wwwroot/js/cards.js
+++ UI/wwwroot/js/cards.js
@@ -1,5 +1,14 @@
-﻿class Card {
-    constructor(img, title, date, type = "", id, user = "") {
+﻿/**
+ * Spotify card
+ * @param {string} img src for img background, if leave empty string then fill with base gradient
+ * @param {string} title title for card
+ * @param {string} date date for card
+ * @param {string} type ["Album", "Album", "Single", "EP", "PLAYLIST"]
+ * @param {int} id id for card
+ * @param {string} user by user for card
+ */
+class Card {
+    constructor(img = "", title, date, type = "", id, user = "") {
         this.img = img;
         this.title = title;
         this.date = !date ? "" : date + " •";
@@ -8,9 +17,12 @@
         this.user = user;
     }
     
+    /**
+     * render created card
+     * @returns {Element} - html element of card
+     */
     render() {
         const card = document.createElement("div");
-            
         card.className = "card";
         card.addEventListener("click", (e) => {
             if (e.target && e.target.classList.contains("button-on-card")) {
@@ -27,9 +39,14 @@
                 <span class="playlist-marker">${this.type}</span>
             </h2>`
         
+        let watermark = this.img === "" ? 
+            `<div class="watermark empty"></div>` :
+            `<img src="../img/${this.img}" alt="" class="watermark"/>
+` 
+        
         card.innerHTML = ` 
             <div class="cover">
-                <img src="../img/${this.img}" alt="" class="watermark"/>
+                ${watermark}
                 <div class="button-on-card"></div> <!--can be made active with "card-active" class-->
                 <div class="cover-text">
                     <h1 class="cover-title">${this.title}</h1>
