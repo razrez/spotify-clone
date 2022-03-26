927bbcc85bc6992c2103db15967bc15e9c17a463
SalavatKhR <SalavatKhR@gmail.com>
2022-03-26 19:49:05 +0300

    Playlist-header

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Playlist()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
