ef7cb4efc708ce8759300e7b34b2f2ac5bb89813
prcstak <g.marsel2002@gmail.com>
2022-04-03 11:00:03 +0300

    add universal-page header

--- UI/wwwroot/js/MainLayoutScript.js
+++ UI/wwwroot/js/MainLayoutScript.js
@@ -1,9 +1,28 @@
-﻿$(document).ready(function(){
-    $('.link').click(function(){
-        if($(this).parent().hasClass('link-active')){
+﻿$(document).ready(function () {
+    $('.layout_content').scroll(function () {
+        $('.header-overlay').css("opacity", 0 + $('.layout_content').scrollTop() / 200)
+    });
+    $('.link').click(function () {
+        if ($(this).parent().hasClass('link-active')) {
             return false;
         }
         $('.link').removeClass('link-active');
         $(this).addClass('link-active');
     });
+    
+    $('.dropbtn').click(function (){
+        $('#dropdown').toggleClass("show");
+    })
+    $(window).click(function () {
+        if (!event.target.matches('.dropbtn')) {
+            let dropdowns = $(".dropdown-content");
+            let i;
+            for (i = 0; i < dropdowns.length; i++) {
+                let openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    })
 });
