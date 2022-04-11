7b31615b0e92ca64c64958522759cad3def92758
Руслан Русланов <sharp_blade666@mail.ru>
2022-04-11 11:55:18 +0300

    Search Page init

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -27,6 +27,12 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    [Route("Search")]
+    public IActionResult Search()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
