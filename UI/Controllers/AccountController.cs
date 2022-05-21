bb6fe58da6794a324f8d498a014fe09727e5659e
prcstak <g.marsel2002@gmail.com>
2022-05-21 19:15:29 +0300

    login + getToken func -> fetch data indexPage

--- UI/Controllers/AccountController.cs
+++ UI/Controllers/AccountController.cs
@@ -1,13 +1,13 @@
 ﻿using System.Diagnostics;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using UI.Models;
 
 namespace UI.Controllers;
 
 public class AccountController : Controller
 {
-    
     public IActionResult Overview()
     {
         return View();
@@ -17,30 +17,39 @@ public class AccountController : Controller
     {
         return View();
     }
-    
+
     [HttpGet]
     public IActionResult LogIn()
     {
         return View();
     }
-    
-    [HttpPost]
-    public IActionResult LogIn(string email, string password, bool remember)
-    {
 
+    [HttpPost]
+    public async Task<IActionResult> LogIn(string email, string password, bool remember)
+    {
         var client = new HttpClient();
         var values = new Dictionary<string, string>()
         {
             {"grant_type", "password"},
-            {"username", "user01@gmail.com"},
-            {"password", "qWe!123"},
+            {"username", email},
+            {"password", password},
         };
         var content = new FormUrlEncodedContent(values);
         var res = client.PostAsync($"https://localhost:7030/login", content);
+
         if (res.Result.StatusCode == HttpStatusCode.OK)
         {
+            var jsonString = await res.Result.Content.ReadAsStringAsync();
+            var token = JsonConvert.DeserializeObject<ResponseType>(jsonString)!.access_token;
+            HttpContext.Response.Cookies.Append(".AspNetCore.Application.Id", value: token,
+                new CookieOptions
+                {
+                    MaxAge = TimeSpan.FromMinutes(60)
+                });
             return RedirectToAction("Index", "Home");
         }
+
+        // TODO wrong login or password 
         return View();
     }
 
@@ -55,4 +64,11 @@ public class AccountController : Controller
     }
 
     public IActionResult ChangePassword() => View();
+}
+
+public class ResponseType
+{
+    public string access_token;
+    public string token_type;
+    public int expires_in;
 }
--- UI/wwwroot/js/IndexPageScript.js
+++ UI/wwwroot/js/IndexPageScript.js
@@ -1,10 +1,9 @@
-let token = "eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZDQkMtSFM1MTIiLCJraWQiOiI1RkNFQzc5QzE1NDUzNUJGMUU3RkQ0NTYzRjU1RTU4MzdCQkZFNEEwIiwidHlwIjoiYXQrand0In0.ZqE5mPZFi8gJT9jy9wT882EOApAzK92mtb3mVKiEM5wEOJ6Pm1vHDV4wJG6wi0hnpEHH0NzseLqKXpVbZxpA3MP7ThhbZ8ZhQuaxQYz3VzH17_mnlsziXf8KZhSaRp_CxztgU7J188TT4881llDqFKOD4LonJQb47lR9IfwG4sHRCGDtIAoBkL2EQ_pwZkZ3QXV86eCanOruw-FO6AEdXJZj26ksUoSX6dPPChsgGqsJ8WrBwOU_mJD3-LoSPYI-ji8uDsUCZmzdBP55te5RQ2CGtBEjVvwXPqK3OxOHZgpdJuXppbOR9Qv_tavWFig2hBeNVshsRpQZlTsZWGGTNw.le2DgnL5soAukE84uUxoLg.ODTfb7HdMhbh_RA-d2ikPVF5QGG4KKJehp5dck9Lz-Bu8tJxvBLTIeimaCXYc_LqZW4Jwhj4bYUGTKyW6RSe6PFVE4U5_XXAIcKLjTz5VyaRV3bfcMEfn2SEx6_IH0wbsuLU7iPgY97nrCD8_IjfH76XsNzkaki919nOEInNvV0pi0-SmjyI5iHrIJiDzDOZkGKygdaEwKgP99mrCX24AUuQbgxoa7VhNN4Rs13U64j5NCbQdge9c3CEnJkYojr_4rzfY1juBMGxU54CiSb_GQDTwA4MjRWweaZR-h7Rtu0sqUoCSWrN2wg0DH5d_b3axyLGejCFw3r2va6no0EtoWC51ytjDkj1CDcbr-mdHztZc9SyIrmy0-2rSjLuPlI8NfTbrYLeRT3P5WMN02Qabj4L5Ng0yZRB-zZaiosWInjU0PTtdVqEiHZ3FgLiaOGT-YtV0J6FitiQJZURboVlpXSpu3yYgp2HYlqgPpb1nXc2YslFzmdutNGHMpzwGi2Wf2K4YlV1nnVfJ8trcu6hjTkhkII2tv8ws5Xek-NNFKMmyI7Oh01QeCTHYooiRpGwZMJWY_mA_yl26LWX87Pk57DnUlGsui8PeTFbYXbejiB1_g3JD7VmusvsYE7EMzEBYY82mbWyvhEpoEOdlIDBdBGl4S8ELSyIzuLsx-2FHTvfDgIA3IwinrdN8WbsmlSW2MWc1l6R3ak434Z5lRKJqagpCWuJGMQGE9Lh8PPeICxtD0MMvf4r08NvE-10D-kF9o4WDxJ6_NPPhCnUi058sKBkGnCNlklMAZw5cKiG4JBCQcCgE5PYJrOK77i4h_BN1amGkkU3J9f-9gLMT9yXWnzelVvJMVfNk1BtHHae4zFzOdptjBtPeUfmovGzx2grBHkK-9of8s1s2OgWIye2xcelQsI9bp2VIPEnT-xpWjrSBwBbZ4CDPhg-Rx1KS_9SVB_oA1ySzIfkcUO2PWpIS3VK1alsUPpjTAx-8GCEGFSzCM-bFW0X2XMHyXsJX2uwOTUSadGWNYyMNVtMfe5k61pSDTknOxSUt9Zo7dd0fqQ.8lcAXAsx1Nn0lsOFLtDs-EB3f0jlTYSF6P790V5RG84";
 function load (){
     let container = document.querySelector("#content");
     fetch(apiHost + "/playlists/random?count=2",{
         method: "GET",
         headers : {
-            'Authorization': `Bearer ${token}`
+            'Authorization': `Bearer ${getToken()}`
         }
     })
         .then(response => response.json())
