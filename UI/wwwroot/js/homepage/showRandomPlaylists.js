c4e8ee342e5864d5cfc4efab1e62a086dfef7506
evgeniy <blackbull0007@gmail.com>
2022-06-12 16:49:23 +0100

    remove token

--- UI/wwwroot/js/homepage/showRandomPlaylists.js
+++ UI/wwwroot/js/homepage/showRandomPlaylists.js
@@ -3,7 +3,7 @@ function showRandomPlaylists (){
     fetch(api + "/home/random/playlists?count=10",{
         method: "GET",
         headers : {
-            'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IkI3Mzc1RUNFMEU5MEZGODg0MDg0Q0UzNjk4QzlBMjFBRDJCQjkzQTgiLCJ4NXQiOiJ0emRlemc2UV80aEFoTTQybU1taUd0SzdrNmciLCJ0eXAiOiJhdCtqd3QifQ.eyJzdWIiOiI1ZjM0MTMwYy0yZWQ5LTRjODMtYTYwMC1lNDc0ZThmNDhiYWMiLCJuYW1lIjoidXNlcjAxQGdtYWlsLmNvbSIsImVtYWlsIjoidXNlcjAxQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwib2lfYXVfaWQiOiIwMjA2NWFjOC1jN2ExLTRmZWMtYTg0Ny1iMGQxMTBiOWIzYjciLCJvaV90a25faWQiOiI2MjMzMzQ2YS0xZjkxLTQxYzQtODZiZS05MTg5MTdkZGIxNWQiLCJzY29wZSI6Im9mZmxpbmVfYWNjZXNzIiwiZXhwIjoxNjU1MDQ5MzkxLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MDMwLyIsImlhdCI6MTY1NTA0NTc5MX0.fChNkaW4N12V8McvMtTXz520Fs1URJnvgt63tGHzPGIH1isxikbOBE49llfMg53dRc52JvIg1kBoKFy0wRA6Z79GvEQnRwFBsuhf-N5KixfMwOYv2YckQMz7NXAfKG8jX7D5iWDoKFTRsEYu7Dtg8vIl1tAavFxdPOfiTmdCM267EGDCrDv6yikjVw1zi_VGajorrrTBJyNYE_6o7T3rzEZrVWmcxP2JmGpJEZ-ijIdilwhvLxUoR1QGO9UHmrMBqcbLxUkOPaqCzl9cDqZ-sXxwUjCHjScjD5oPYoaWR_2uXP7C-IkQ8r2C3rTcYxaFjzjDeCf_ISQJaYADKOsAHg`
+            'Authorization': `Bearer ${getToken()}`
         }
     })
         .then(response => response.json())
