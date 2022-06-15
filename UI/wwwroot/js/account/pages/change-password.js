c6041903221c4b524be0751b2348f742dcf96d78
Руслан Русланов <sharp_blade666@mail.ru>
2022-06-15 04:57:58 +0300

    change-password fixed

--- UI/wwwroot/js/account/pages/change-password.js
+++ UI/wwwroot/js/account/pages/change-password.js
@@ -1,26 +1,25 @@
-document.getElementById('change-password')
-    .addEventListener("submit", async function(event) {
-        event.preventDefault();
-        await getClaims().then( async claims => {
-            const changePasswordUrl = "https://localhost:7030/api/profile/password/change";
-            const changeData = {
-                userId: claims['id'],
-                oldPassword: event.target['password'].value,
-                newPassword: event.target['new-password'].value
-            };
-            try {
-                await changePasswordAsync(changePasswordUrl, changeData)
-                    .then((data) => {
-                        console.log(data);
-                        document.querySelector("#result").appendChild(data);
-                    });
-            }
-            catch (error){
-                console.log('wrong password');
-                document.getElementById('result').innerHTML = "wrong credentials or new password isn't safe";
-                document.getElementById('reset').click();
-                //window.location.reload();
-            }
+document.getElementById('change-password').addEventListener("submit", async function(event) {
+    event.preventDefault();
+    await getClaims().then( async claims => {
+        const changePasswordUrl = "https://localhost:7030/api/profile/password/change";
+        const changeData = {
+            userId: claims['id'],
+            oldPassword: event.target['password'].value,
+            newPassword: event.target['new-password'].value
+        };
+        try {
+            await changePasswordAsync(changePasswordUrl, changeData)
+                .then((data) => {
+                    console.log(data);
+                    document.querySelector("#result").innerHTML = data;
+                });
+        }
+        catch (error){
+            console.log('fail for changing password');
+            document.getElementById('result').innerHTML = "wrong credentials or new password isn't safe";
+            document.getElementById('reset').click();
+            //window.location.reload();
+        }
 
     });
 })
@@ -35,7 +34,7 @@ async function changePasswordAsync (url, data) {
         body: JSON.stringify(data)
     });
 
-    if (response.status !== 200){
+    if (response.status > 300){
         throw new Error();
     }
 
